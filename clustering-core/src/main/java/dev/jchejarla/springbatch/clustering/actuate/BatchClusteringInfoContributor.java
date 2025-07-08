4be80cfd98943bcf38ea8ff16b660fa295d28c5f
jchejarla <jchejarla@outlook.com>
2025-07-07 20:25:50 -0400

    Preparing for 1.0.0 release

--- clustering-core/src/main/java/dev/jchejarla/springbatch/clustering/actuate/BatchClusteringInfoContributor.java
+++ clustering-core/src/main/java/dev/jchejarla/springbatch/clustering/actuate/BatchClusteringInfoContributor.java
@@ -12,7 +12,7 @@ import java.util.Map;
 @ConditionalOnClusterEnabled
 public class BatchClusteringInfoContributor implements InfoContributor {
 
-    private static final String VERSION = "1.0-SNAPSHOT";
+    private static final String VERSION = "1.0.0";
 
     private final BatchClusterProperties batchClusterProperties;
 
